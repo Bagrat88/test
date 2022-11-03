0cdbf6da803f07c4a29d271bae0191e2fecdf9c3
Bagrat <bhovsepyan88@gmail.com>
2022-11-03 14:31:43 +0300

    Дедлайн

---
+++ mysite/news/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render, redirect
+from .models import News, Category
+from .forms import UserRegisterForm, UserLoginForm
+from django.contrib import messages
+from django.contrib.auth import login, logout
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'Вы успешно зарегистрировались')
+            return redirect('home')
+        else:
+            messages.error(request, 'Ошибка регистрации')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'news/register.html', {"form": form})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserLoginForm
+    return render(request, 'news/login.html', {"form": form})
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
+
+
+def index(request):
+    news = News.objects.all()
+    context = {
+        'news': news,
+        'title': 'список новостей',
+    }
+    return render(request, 'news/index.html', context)
+
+
+def get_category(request, category_id):
+    news = News.objects.filter(category_id=category_id)
+    category = Category.objects.get(pk=category_id)
+    return render(request, 'news/category.html', {'news': news, 'category': category})
